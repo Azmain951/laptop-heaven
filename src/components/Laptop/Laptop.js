ed267bef88457ab22c0ee0b246350660503ec226
Afridi Azmain <Azmain951>
2022-03-25 20:46:54 +0600

    laptop details added and css styles added on components

--- src/components/Laptop/Laptop.js
+++ src/components/Laptop/Laptop.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import './Laptop.css'
 
 const Laptop = ({ laptop }) => {
     const { name, img, price } = laptop;
     return (
-        <div>
+        <div className='laptop-container'>
             <img src={img} alt="" />
+            <p>{name}</p>
+            <p>Price: ${price}</p>
         </div>
     );
 };
--- src/components/Laptops/Laptops.js
+++ src/components/Laptops/Laptops.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Laptop from '../Laptop/Laptop';
+import './Laptops.css'
 
 const Laptops = () => {
 
@@ -13,13 +14,15 @@ const Laptops = () => {
     return (
         <div>
             <h3>Welcome to Laptop Heaven</h3>
-            <div className="laptops-container">
-                {
-                    laptops.map(laptop => <Laptop key={laptop.id} laptop={laptop}></Laptop>)
-                }
-            </div>
-            <div className="cart-container">
-                <h3>This is Cart Container</h3>
+            <div className='shop'>
+                <div className="laptops-container">
+                    {
+                        laptops.map(laptop => <Laptop key={laptop.id} laptop={laptop}></Laptop>)
+                    }
+                </div>
+                <div className="cart-container">
+                    <h3>This is Cart Container</h3>
+                </div>
             </div>
         </div>
     );
