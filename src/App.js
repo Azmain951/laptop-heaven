deadefaa8ec50695533e243a815dedaad9bc21e2
Afridi Azmain <Azmain951>
2022-03-26 00:20:48 +0600

    fake data updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-import logo from './logo.svg';
 import './App.css';
 import Laptops from './components/Laptops/Laptops';
 import Navbar from './components/Navbar/Navbar';
 import Questions from './components/Questions/Questions';
-import { useState } from 'react';
 
 function App() {
   return (
--- src/components/Wishlist/Wishlist.js
+++ src/components/Wishlist/Wishlist.js
@@ -2,11 +2,12 @@ import React from 'react';
 import './Wishlist.css'
 
 const Wishlist = ({ item }) => {
-    const { name, img } = item;
+    const { name, img, price } = item;
     return (
         <div className='wishlist'>
             <img src={img} alt="" />
             <p>{name}</p>
+            <p>${price}</p>
         </div>
     );
 };
